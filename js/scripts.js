236fb8c5c0817ca992ba68477015f23303c10c96
Tsaikkari <trospe@gmx.de>
2019-11-14 08:01:49 +0100

    Change colors

--- js/scripts.js
+++ js/scripts.js
@@ -16,17 +16,17 @@ $(document).ready(function() {
       for(var i = 0; i < works.length; ++i) {
         $("#work").append("\
         <div class='col-xs-12 col-sm-6 col-md-4'>\
-        <a href='" + works[i].href + "' class='work-pic'>\
-          <img class='img-responsive' src='" + works[i].pic + "'>\
+          <a href='" + works[i].href + "' class='work-pic'>\
+            <img class='img-responsive' src='" + works[i].pic + "'>\
             <span class='info'><p class='proj-title'></p>" + works[i].title + "</span>\
           </a>\
         </div>\
       ");
       var images = $("#work img");
       if(i%2 === 0){
-        $(images[i]).css("border", "2px solid #c0b2b5");
+        $(images[i]).css("border", "2px solid #2b2b45");
       } else {
-        $(images[i]).css("border", "2px solid #d72c16");
+        $(images[i]).css("border", "2px solid #19cbd0");
       };
     };
       $(".work-pic").mouseenter(function() {
@@ -34,5 +34,5 @@ $(document).ready(function() {
       }).mouseleave(function() {
         $(".info", this).hide();
       });
-      return false;
+      return false; 
   });
