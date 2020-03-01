84faf38c3710815b115c726dccb415d0e1db23a6
Tsaikkari <trospe@gmx.de>
2020-03-01 16:32:56 +0100

    Update

--- js/work.js
+++ js/work.js
@@ -2,7 +2,7 @@ var works = [
   {
     title:"Notes Game",
     pic: "img/game.png",
-    href: "https://notes-game-imt6ych5g.now.sh/"
+    href: "https://notes-game-df4wat2zw.now.sh/"
   },
   {
     title:"An E-Commerce Web App with Ruby on Rails",
