051e498346892756fc1932eea0216e48dcbf35bd
Tsaikkari <trospe@gmx.de>
2020-03-01 15:02:44 +0100

    Update

--- js/work.js
+++ js/work.js
@@ -1,12 +1,12 @@
 var works = [
   {
-    title:"An E-Commerce Web App with Ruby on Rails",
-    pic: "img/bike.png",
-    href: "https://coralcat.herokuapp.com"
-  },
-  {
     title:"Notes Game",
     pic: "img/game.png",
     href: "https://notes-game-imt6ych5g.now.sh/"
+  },
+  {
+    title:"An E-Commerce Web App with Ruby on Rails",
+    pic: "img/bike.png",
+    href: "https://coralcat.herokuapp.com"
   }
 ];
