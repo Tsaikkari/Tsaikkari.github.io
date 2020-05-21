03558d2ef1d1a06e275d11096cd560cebfa5129f
Tsaikkari <trospe@gmx.de>
2020-05-21 14:30:28 +0200

    Changing the layout and styles

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
   // Smooth scrolling
-    var $root = $('html, body');
+    const $root = $('html, body');
     $('.navbar a:not(.dropdown-toggle)').click(function() {
-    var href = $.attr(this, 'href');
+    $('.rays').hide();
+    let href = $.attr(this, 'href');
     if (href != undefined && href != '#') {
       $root.animate({
         scrollTop: $(href).offset().top
@@ -11,39 +12,18 @@ $(document).ready(function() {
         });
       };
     });
-
     //work section
-      for(var i = 0; i < works.length; ++i) {
-        $("#work").append("\
-        <div class='col-xs-12 col-sm-6'>\
-        <a href='" + works[i].href + "' class='work-img'>\
-        <img class='img-fluid' src='" + works[i].pic + "'>\
-        <span class='info'><p class='proj-title'></p>" + works[i].title + "</span>\
-        </a>\
-        </div>\
-      ");
-
-      var images = $("#work img");
-      if(i%2 === 0){
-        $(images[i]).css("border", "1px solid #19cbd0");
-      } else {
-        $(images[i]).css("border", "1px solid  #1aa8db");
-      };
-
-      var texts = $(".info");
-      if(i%2 === 0) {
-        $(texts[i]).css({"color": "gold", "top": "6rem", "left": "7.8rem"}); 
-      } else {
-        $(texts[i]).css({"color": "#ff80ed", "top": "2.5rem", "left": "0.5rem"});
-      }
+    for(var i = 0; i < works.length; ++i) {
+      $("#work").append(`<div class='col-lg-3 col-md-6 col-mb-4'><div class='card h-100'>
+      <img class='card-img-top' src='${works[i].pic}'><div class='card-body'>
+      <h4 class='card-title'>${works[i].title}</h4>
+      <p class='card-text'>${works[i].description}</p>
+      </div>
+      <div class='card-footer'>
+      <a href='${works[i].href}' class='btn btn-dark'>Go To App</a>
+      </div>`);
     };
-      $(".work-img").mouseenter(function() {
-        $(".info", this).show();
-      }).mouseleave(function() {
-        $(".info", this).hide();
-      });
-
-      return false;
+    return false;
   });
 
   document.getElementById("copyright").innerHTML = new Date().getFullYear();
--- js/work.js
+++ js/work.js
@@ -1,12 +1,26 @@
-var works = [
+let works = [
   {
-    title:"Notes Game",
+    title: "Notes Game",
     pic: "img/game.png",
+    description: "JavaScript App with Material Kit",
     href: "https://notes-game.now.sh/"
   },
   {
-    title:"An E-Commerce Web App with Ruby on Rails",
+    title:"Indecision App",
+    pic: "img/indecision-app.png",
+    description: "React App",
+    href: "https://decisions-decisions.herokuapp.com/"
+  },
+  {
+    title: "Colorful Lives",
+    pic: "img/shades-of-grey.png",
+    description: "JavaScript App with EJS and Node.js",
+    href: ""
+  },
+  {
+    title: "Bike Store",
     pic: "img/bike.png",
+    description: "E-Commerce Web App with Ruby on Rails",
     href: "https://coralcat.herokuapp.com"
   }
 ];
