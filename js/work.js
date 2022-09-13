f63095c593964994b6c987da8bd4b081da7a9c56
Kirsi Trospe <kirsi.trospe@gmail.com>
2022-09-13 09:12:39 +0200

    modified works

--- js/work.js
+++ js/work.js
@@ -7,14 +7,14 @@ let works = [
     href: 'https://digihappy.fi/',
     buttonText: 'Go to App!',
   },
-  {
-    title: 'Cake Shop',
-    pic: 'img/cake-shop.png',
-    description:
-      'FullStack App with Nodejs, MongoDB, Mongoose, React.js and TypeScript',
-    href: 'https://cake-shop-app-kirsi.netlify.app/',
-    buttonText: 'Go to App!',
-  },
+  // {
+  //   title: 'Cake Shop',
+  //   pic: 'img/cake-shop.png',
+  //   description:
+  //     'FullStack App with Nodejs, MongoDB, Mongoose, React.js and TypeScript',
+  //   href: 'https://cake-shop-app-kirsi.netlify.app/',
+  //   buttonText: 'Go to App!',
+  // },
   {
     title: 'Daily Dose of Dopamine',
     pic: 'img/vision-board.png',
@@ -36,11 +36,11 @@ let works = [
     href: 'https://notes-game.now.sh/',
     buttonText: 'Go to App!',
   },
-  {
-    title: 'Get Together',
-    pic: 'img/community-app.png',
-    description: 'Node.js, Express, HBS App',
-    href: 'https://community-app-get-together.herokuapp.com/',
-    buttonText: 'Go to App!',
-  },
+  // {
+  //   title: 'Get Together',
+  //   pic: 'img/community-app.png',
+  //   description: 'Node.js, Express, HBS App',
+  //   href: 'https://community-app-get-together.herokuapp.com/',
+  //   buttonText: 'Go to App!',
+  // },
 ]
