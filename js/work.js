086821a3ff419fcbbf8291794185443dda7e5326
Tsaikkari <kirsi.trospe@gmail.com>
2022-12-30 02:00:02 +0100

    update text

--- js/work.js
+++ js/work.js
@@ -7,7 +7,7 @@ let works = [
     href: 'https://digihappy.fi/',
     buttonText: 'Go to App!',
   },
-  {
+  /*{
     title: 'Cake Shop',
     pic: 'img/cake-shop.png',
     description:
@@ -28,19 +28,19 @@ let works = [
     description: 'React.js App with Redux',
     href: 'https://learn-words-now.herokuapp.com/',
     buttonText: 'Go to App!',
-  },
+  },*/
   {
     title: 'Notes Game',
     pic: 'img/game.png',
     description: 'JavaScript App with Material Kit',
     href: 'https://notes-game.now.sh/',
     buttonText: 'Go to App!',
-  },
+  },/*
   {
     title: 'Get Together',
     pic: 'img/community-app.png',
     description: 'Node.js, Express, HBS App',
     href: 'https://community-app-get-together.herokuapp.com/',
     buttonText: 'Go to App!',
-  },
+  },*/
 ]
